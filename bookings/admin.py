496ed0805257dc866ca1c18b1092387733945003
OisinWrites <oisin.banville@gmail.com>
2023-05-01 14:58:18 +0000

    Register models in admin.py

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Booking, Table
 
 # Register your models here.
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'table',
+        'start_time',
+        'end_time',
+        'email',
+        'user_name',
+        'size_of_party',
+    )
+
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = (
+        'number',
+        'size',
+        'is_available',
+    )
+
+
+# Register your models here.
+admin.site.register(Booking, BookingAdmin,)
+admin.site.register(Table, TableAdmin,)
