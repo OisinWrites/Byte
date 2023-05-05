3f0ea0ab7c7c0284f8110f4c69a19f8865026933
OisinWrites <oisin.banville@gmail.com>
2023-05-05 10:50:34 +0000

    Import static

--- byte/urls.py
+++ byte/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
