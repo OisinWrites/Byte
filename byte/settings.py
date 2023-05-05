1839711894e0f6f333fff9248f3ec4d3ee040325
OisinWrites <oisin.banville@gmail.com>
2023-05-05 10:58:03 +0000

    toggle debug, reveal secret key

--- byte/settings.py
+++ byte/settings.py
@@ -27,10 +27,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'Shhs,_itS0sEcret~'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['byte-restaurant-bookings.herokuapp.com']
 
 
