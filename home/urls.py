a6f5118a00193f9db8a64eab9c361c1641bde53d
OisinWrites <oisin.banville@gmail.com>
2023-05-05 07:57:17 +0000

    Create hamburger and links to pages

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('menu/', views.menu, name='menu'),
 ]
